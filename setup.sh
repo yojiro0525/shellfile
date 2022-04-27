e4417e25b8f7a6f0f4b01bf1f9df6d023fd4468c
yojiro0525 <yojiro0525@outlook.jp>
2022-04-27 14:25:36 +0900

    20220427

--- setup.sh
+++ setup.sh
@@ -6,18 +6,6 @@ sudo apt upadete -y
 sudo apt upgrade -y
 sudo apt -y autoremove
 #-----------------------------------
-#GUIのダウンロード
-sudo apt install -y libgl1-mesa-dev xorg-dev xbitmaps x11-apps
-echo 'export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '\''{print $2}'\''):0.0 #https://astherier.com/blog/2020/08/run-gui-apps-on-wsl2/' >> ~/.profile
-cat << 'EOS' | sudo tee /etc/fonts/local.conf
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-    <dir>/mnt/c/Windows/Fonts</dir>
-</fontconfig> 
-<!-- Created by https://astherier.com/blog/2020/08/run-gui-apps-on-wsl2/ -->
-EOS
-#-----------------------------------
 #unzipのダウンロード
 sudo apt install -y unzip
 #-----------------------------------
