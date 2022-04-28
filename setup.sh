74cef43e890e8353d71372bf1b7a010720c22bf7
yojiro0525 <yojiro0525@outlook.jp>
2022-04-28 12:20:12 +0900

    2022/04/28

--- setup.sh
+++ setup.sh
@@ -9,6 +9,18 @@ sudo apt -y autoremove
 #unzipのダウンロード
 sudo apt install -y unzip
 #-----------------------------------
+#GUI環境
+sudo apt install -y libgl1-mesa-dev xorg-dev xbitmaps x11-apps
+echo 'export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '\''{print $2}'\''):0.0 #https://astherier.com/blog/2020/08/run-gui-apps-on-wsl2/' >> ~/.profile
+cat << 'EOS' | sudo tee /etc/fonts/local.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <dir>/mnt/c/Windows/Fonts</dir>
+</fontconfig> 
+<!-- Created by https://astherier.com/blog/2020/08/run-gui-apps-on-wsl2/ -->
+EOS
+#-----------------------------------
 #denoのインストール
 curl -fsSL https://deno.land/x/install/install.sh | sh
 echo 'export DENO_INSTALL="/home/$USER/.deno"' >> ~/.bashrc
@@ -20,13 +32,9 @@ sudo add-apt-repository ppa:jonathonf/vim
 sudo apt update -y
 sudo apt install -y vim
 #-----------------------------------
-#vimの設定
-cd 
-git clone https://github.com/yojiro0525/vimfile.git .
-#-----------------------------------
 #pythonのインストール
 cd
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 sudo pip3 install numpy
 sudo pip3 install matplotlib
 sudo pip3 install scipy
